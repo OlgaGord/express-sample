c2a5417db792c7b2ee46d09a147c56c222014d33
unknown <olga1gordeyeva@gmail.com>
2019-04-16 09:04:20 -0400

    Tiny editor added

--- app.js
+++ app.js
@@ -8,53 +8,50 @@ const bodyParser = require("body-parser");
 const app = express();
 const dbLayer = require("./config/db");
 
-const rootRouter = require("./routes/root");
+const pagesRouter = require("./routes/pages");
 const usersRouter = require("./routes/users");
 const numbersRouter = require("./routes/numbers");
 
 const port = 9000;
 
-
-
 app.set("view engine", "hbs");
 hbs.registerPartials(path.join(__dirname, "views/partials"));
 
-
 app.use(bodyParser.json({ limit: "150mb" }));
 app.use(
-	bodyParser.urlencoded({
-		// to support URL-encoded bodies
-		limit: "150mb",
-		extended: true
-	})
+  bodyParser.urlencoded({
+    // to support URL-encoded bodies
+    limit: "150mb",
+    extended: true
+  })
 );
 
 app.use(
-	fileUpload({
-		useTempFiles: true,
-		tempFileDir: "/tmp/"
-	})
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/"
+  })
 );
 
 app.use(cookieParser());
 app.use("/inc", express.static(path.join(__dirname, "inc")));
 
 app.use((req, res, next) => {
-	if (req.cookies.vcount == undefined) {
-		req.vcount = 1;
-		res.cookie("vcount", 1);
-	} else {
-		req.vcount = parseInt(req.cookies.vcount) + 1;
-		res.cookie("vcount", req.vcount);
-	}
-	next();
+  if (req.cookies.vcount == undefined) {
+    req.vcount = 1;
+    res.cookie("vcount", 1);
+  } else {
+    req.vcount = parseInt(req.cookies.vcount) + 1;
+    res.cookie("vcount", req.vcount);
+  }
+  next();
 });
 app.use("/users", usersRouter);
-app.use("/", rootRouter);
+app.use("/", pagesRouter);
 
 app.use("/numbers", numbersRouter);
 
 app.listen(port, function() {
-	dbLayer.init();
-	debug("Sample App " + port + "!");
+  dbLayer.init();
+  debug("Sample App " + port + "!");
 });
--- model/page.js
+++ model/page.js
@@ -1,13 +1,28 @@
 const db = require("../config/db");
 
 module.exports = class {
-	static async getPage(pageKey) {
-		let connection = await db.getConnection();
-		const rows = await connection.query(
-			"SELECT * FROM page WHERE pageKey = ?",
-			[pageKey]
-		);
-		connection.end();
-		return rows;
-	}
+  static async getPage(pageKey) {
+    let connection = await db.getConnection();
+    const rows = await connection.query(
+      "SELECT * FROM page WHERE pageKey = ?",
+      [pageKey]
+    );
+    connection.end();
+    return rows;
+  }
+
+  static async savePage(pageKey, title, content) {
+    let connection = await db.getConnection();
+    // let q =
+    //   "INSERT INTO page (pageKey, title, content) values(?, ?, ?) ON DUPLICATE KEY UPDATE title=?, content=?";
+    // console.log(q, pageKey, title, content);
+    const result = await connection.query(
+      "INSERT INTO page (pageKey, title, content) values(?, ?, ?) ON DUPLICATE KEY UPDATE " +
+        "title=?, content=?",
+      [pageKey, title, content, title, content]
+    );
+
+    connection.end();
+    return result;
+  }
 };
